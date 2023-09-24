e6d0896edc513b19c3544d4af71438ff5f46d321
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-09-24 21:48:47 +0300

    Books view model

---
+++ BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.DBOperations;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Book> Handle() 
+        {
+            var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList<Book>();
+
+            return bookList;
+        }
+    }
+
+    public class BooksViewModel
+    {
+        public string Title { get; set; }
+
+        public int PageCount { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
