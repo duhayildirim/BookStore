1d084c35eba69c4c637f503e0c247411ba8174e8
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-10-04 10:45:09 +0300

    exception middleware

---
+++ Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace BookStore.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string message = "[Request] HTTP" + context.Request.Method + " - " + context.Request.Path;
+            Console.WriteLine(message);
+            await _next(context);
+
+
+        }
+    }
+
+    public static class CustomExceptionMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddle(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleware>();
+        }
+    }
+}
